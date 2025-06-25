c7dbd64b285db3758067ea57dd117367f79567ca
yukifury <forlorn128@gmail.com>
2025-06-25 03:03:02 +0100

    fix: fix eslint error on `.module.css` and `.css` files;

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
-/// <reference types="vite/client" />
+// <reference types="vite/client" />
+
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+
+  export default classes;
+}
+
+declare module '*.css' {
+  const classes: { [key: string]: string };
+
+  export default classes;
+}
